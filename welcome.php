297c48bb4d80f0b2d6e70c6f0432b5a2e6cf86f7
LowZhong <lowzhong203056@dasein.edu.my>
2022-04-12 10:36:09 +0800

    new php files

---
+++ welcome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container-fluid h1 text-center m-2" class="text-primary">>
+        <?php
+        echo "Welcome!"."<br/>";
+        echo $_GET["username"] . "<br/>";
+        ?>
+    </div>
+</body>
+</html>
