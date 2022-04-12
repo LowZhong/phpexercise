b631a44c84476db50318f6ea6d88373d8c2f8ad5
LowZhong <lowzhong203056@dasein.edu.my>
2022-04-13 00:17:45 +0800

    dashboard done

--- welcome.php
+++ welcome.php
@@ -29,7 +29,7 @@
         // delete message prompt will be here
 
         // select all data
-        $query = "SELECT customer.customerID, COUNT(order_summary.orderID) AS NumberOfOrders
+        $query = "SELECT customer.customerID, SUM(order_summary.orderID) AS NumberOfOrders
         FROM order_summary
         LEFT JOIN customer ON order_summary.CustomerID = customer.customerID
         GROUP BY customerID;";
@@ -80,11 +80,11 @@
         <?php
 
         // select all data
-        $query = "SELECT order_details.productID , products.name
-                    FROM order_details
-                    RIGHT JOIN products
-                    ON order_details.productID = products.productID
-                    WHERE order_details.productID is NULL";
+        $query = "SELECT products.productID 
+        FROM products
+        LEFT JOIN order_details 
+        ON products.productID = order_details.productID
+        WHERE order_details.productID is NULL";
         $stmt = $con->prepare($query);
         $stmt->execute();
 
@@ -100,7 +100,7 @@
             //creating our table heading
             echo "<tr>";
             echo "<th>Product ID</th>";
-            echo "<th>Product Name</th>";
+            //echo "<th>Product Name</th>";
             echo "</tr>";
 
             // table body will be here
@@ -115,7 +115,7 @@
                 // creating new table row per record
                 echo "<tr>";
                 echo "<td>{$productID}</td>";
-                echo "<td>{$name}</td>";
+                //echo "<td>{$name}</td>";
                 echo "</td>";
                 echo "</tr>";
             }
@@ -133,7 +133,66 @@
         <?php
 
         // select all data
-        $query = "SELECT ";
+        $query = "SELECT customer.customerID
+                    FROM customer
+                    LEFT JOIN order_summary
+                    ON customer.customerID = order_summary.customerID
+                    WHERE order_summary.customerID is NULL;";
+        $stmt = $con->prepare($query);
+        $stmt->execute();
+
+        // this is how to get number of rows returned
+        $num = $stmt->rowCount();
+
+        //check if more than 0 record found
+        if ($num > 0) {
+
+            // data from database will be here
+            echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
+
+            //creating our table heading
+            echo "<tr>";
+            echo "<th>Customer ID</th>";
+            //echo "<th>Customer Name</th>";
+            echo "</tr>";
+
+            // table body will be here
+            // retrieve our table contents
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // extract row
+                // this will make $row['firstname'] to just $firstname only
+
+                extract($row);
+                //function
+
+                // creating new table row per record
+                echo "<tr>";
+                echo "<td>{$customerID}</td>";
+                //echo "<td>{$}</td>";
+                echo "</td>";
+                echo "</tr>";
+            }
+            // end table
+            echo "</table>";
+        }
+
+        // if no records found
+        else {
+            echo "<div class='alert alert-danger'>No records Order ID found.</div>";
+        }
+        ?>
+
+        <h3>Top 3 best selling products</h3>
+        <?php
+
+        // select all data
+        $query = "SELECT products.productID, products.name, SUM(order_details.Quantity) AS ProductTotals
+                    FROM products
+                    INNER JOIN order_details
+                    ON products.productID = order_details.productID
+                    GROUP BY products.productID
+                    ORDER BY ProductTotals DESC
+                    LIMIT 3;";
         $stmt = $con->prepare($query);
         $stmt->execute();
 
@@ -149,7 +208,8 @@
             //creating our table heading
             echo "<tr>";
             echo "<th>Product ID</th>";
-            echo "<th>Product Name</th>";
+            echo "<th>product name</th>";
+            echo "<th>Product Totals</th>";
             echo "</tr>";
 
             // table body will be here
@@ -165,6 +225,7 @@
                 echo "<tr>";
                 echo "<td>{$productID}</td>";
                 echo "<td>{$name}</td>";
+                echo "<td>{$ProductTotals}</td>";
                 echo "</td>";
                 echo "</tr>";
             }
