e9caf9c86dd9038114027c567ec5e5ca5880ba1f
LowZhong <lowzhong203056@dasein.edu.my>
2022-04-19 02:15:33 +0800

    minor change

---
+++ delete_product.php
@@ -0,0 +1,29 @@
+<?php
+// include database connection
+include 'database/connection.php';
+
+
+try {
+    // get record ID
+    // isset() is a PHP function used to verify if a value is there or not
+    $productID = isset($_GET['productID']) ? $_GET['productID'] :  die('ERROR: Record ID not found.');
+
+    // delete query
+    $query = "DELETE FROM products WHERE productID = ?";
+    $stmt = $con->prepare($query);
+    $stmt->bindParam(1, $productID);
+
+    if ($stmt->execute()) { 
+        // redirect to read records page and
+        // tell the user record was deleted
+        header('Location: product_read.php?action=deleted');
+    } else {
+        die('Unable to delete record.');
+    }
+}
+// show error
+catch (PDOException $exception) {
+    die('ERROR: ' . $exception->getMessage());
+}
+?>
+
