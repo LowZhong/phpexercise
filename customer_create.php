aff76c4ecb979e2342b7cd85977fec5f9fa1a360
LowZhong <lowzhong203056@dasein.edu.my>
2022-04-19 20:09:22 +0800

    minor change

--- customer_create.php
+++ customer_create.php
@@ -63,35 +63,6 @@
                         : "";
                     $user_image = htmlspecialchars(strip_tags($user_image));
 
-                    if ($user_image) {
-                        $target_directory = "uploads/";
-                        // make sure the 'uploads' folder exists
-                        // if not, create it
-                        if (!is_dir($target_directory)) {
-                            mkdir($target_directory, 0777, true);
-                        }
-                        $target_file = $target_directory . $user_image;
-
-                        // make sure file does not exist
-                        if (file_exists($target_file)) {
-                            $file_upload_error_messages .= "<div>Image already exists. Try to change file name.</div>";
-                        }
-
-                        // check the extension of the upload file
-                        $file_type = pathinfo($target_file, PATHINFO_EXTENSION);
-                        // make sure certain file types are allowed
-                        $allowed_file_types = array("jpg", "png");
-                        if (!in_array($file_type, $allowed_file_types)) {
-                            $file_upload_error_messages .= "<div>Only JPG and PNG files are allowed.</div>";
-                        }
-                        // make sure submitted file is not too large
-                        if ($_FILES['user_image']['size'] > (5120)) {
-                            $file_upload_error_messages .= "<div>Image must be less than 5 MB in size.</div>";
-                        }
-                    } else {
-                        echo "no file selected.";
-                    }
-
                     // bind the parameters
                     $stmt->bindParam(':username', $username);
                     $stmt->bindParam(':password', $password);
@@ -107,32 +78,66 @@
                     // Execute the query
                     if ($stmt->execute()) {
                         echo "<div class='alert alert-success'>Record was saved.</div>";
+                        if ($user_image) {
+                            $target_directory = "uploads/";
+                            // make sure the 'uploads' folder exists
+                            // if not, create it
+                            if (!is_dir($target_directory)) {
+                                mkdir($target_directory, 0777, true);
+                            }
+                            $target_file = $target_directory . $user_image;
+
+                            // make sure file does not exist
+                            if (file_exists($target_file)) {
+                                $file_upload_error_messages .= "<div>Image already exists. Try to change file name.</div>";
+                            }
+
+                            // check the extension of the upload file
+                            $file_type = pathinfo($target_file, PATHINFO_EXTENSION);
+                            // make sure certain file types are allowed
+                            $allowed_file_types = array("jpg", "png");
+                            if (!in_array($file_type, $allowed_file_types)) {
+                                $file_upload_error_messages .= "<div>Only JPG or PNG files are allowed.</div>";
+                            }
+                            // make sure submitted file is not too large
+                            if ($_FILES['user_image']['size'] > (5242880)) {
+                                $file_upload_error_messages .= "<div>Image must be less than 5 MB in size.</div>";
+                            }
+                            // make sure the 'uploads' folder exists
+                            // if not, create it
+                            if (!is_dir($target_directory)) {
+                                mkdir($target_directory, 0777, true);
+                            }
+                            // if $file_upload_error_messages is still empty
+                            if (empty($file_upload_error_messages)) {
+                                // it means there are no errors, so try to upload the file
+                                if (move_uploaded_file($_FILES["user_image"]["tmp_name"], $target_file)) {
+                                    // it means photo was uploaded
+                                } else {
+                                    echo "<div class='alert alert-danger'>";
+                                    echo "<div>Unable to upload photo.</div>";
+                                    echo "<div>Update the record to upload photo.</div>";
+                                    echo "</div>";
+                                }
+                            }
+                            // if $file_upload_error_messages is NOT empty
+                            else {
+                                // it means there are some errors, so show them to user
+                                echo "<div class='alert alert-danger'>";
+                                echo "<div>{$file_upload_error_messages}</div>";
+                                echo "<div>Update the record to upload photo.</div>";
+                                echo "</div>";
+                            }
+                        } else {
+                            echo "no file selected.";
+                        }
                     } else {
                         echo "<div class='alert alert-danger'>Unable to save record.</div>";
                     }
                 }
-
                 // show error
                 catch (PDOException $exception) {
                     die('ERROR: ' . $exception->getMessage());
-                }
-
-                if (empty($file_upload_error_messages)) {
-                    // it means there are no errors, so try to upload the file (now only start uploading)
-                    if (move_uploaded_file($_FILES["user_image"]["tmp_name"], $target_file)) {
-                        echo "<div class='alert'>";
-                        echo "<div>Uploaded successfully</div>";
-                        echo "</div>";
-                    } else {
-                        echo "<div class='alert alert-danger'>";
-                        echo "<div>Unable to upload photo.</div>";
-                        echo "</div>";
-                    }
-                } else {
-                    // it means there are some errors, so show them to user
-                    echo "<div class='alert alert-danger'>";
-                    echo "<div>{$file_upload_error_messages}</div>";
-                    echo "</div>";
                 }
             } else {
                 foreach ($error as $value) {
