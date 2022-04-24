fabbe81b04539ac6cbf7dc7aad1bbad6b942dabc
LowZhong <lowzhong203056@dasein.edu.my>
2022-04-24 23:12:09 +0800

    minor change

--- order_create.php
+++ order_create.php
@@ -27,122 +27,113 @@
             $customerID = $_POST['customerID'];
             $productID = $_POST['productID'];
             $quantity = $_POST['quantity'];
+            echo count($productID);
+            var_dump($productID);
+            var_dump($quantity);
 
+            echo $customerID;
+            if (!empty($customerID)) {
+                if (!empty($productID)) {
 
-            $name = "";
-            $error['username'.'name'.'quantity'] = validateOrderusername($username, $name, $quantity);
-            $error['name'] = validateOrderProduct($productID, $name);
-            $error = array_filter($error);
-            if (empty($error)) {
+                    try {
+                        // insert query
+                        $query = "INSERT INTO order_summary (customerID) VALUES (?)";
+                        // prepare query for execution
+                        $stmt = $con->prepare($query);
+                        // bind the parameters
+                        $stmt->bindParam(1, $customerID);
+                        // Execute the query
+                        if ($stmt->execute()) {
+                            $last_order_id = $con->lastInsertId();
+                            if ($last_order_id > 0) {
 
-                try {
-                    // insert query
-                    $query = "INSERT INTO order_summary (customerID) VALUES (?)";
-                    // prepare query for execution
-                    $stmt = $con->prepare($query);
-                    // bind the parameters
-                    $stmt->bindParam(1, $customerID);
-                    // Execute the query
-                    if ($stmt->execute()) {
-                        $last_order_id = $con->lastInsertId();
-                        if ($last_order_id > 0) {
+                                for ($i = 0; $i < count($productID); $i++) {
 
-                            for ($i = 0; $i < count($productID); $i++) {
+                                    try {
+                                        $query = "INSERT INTO order_details (orderID, productID, quantity) VALUES (:lastorderid, :productID, :quantity)";
 
-                                try {
-                                    $query = "INSERT INTO order_details (orderID, productID, quantity) VALUES (:lastorderid, :productID, :quantity)";
-
-                                    //prepare query for execute
-                                    $stmt = $con->prepare($query);
-                                    //posted values
-                                    $stmt->bindParam(":lastorderid", $last_order_id);
-                                    $stmt->bindParam(":productID", $productID[$i]);
-                                    $stmt->bindParam(":quantity", $quantity[$i]);
-                                    //execute the query
-                                    if ($stmt->execute()) {
+                                        //prepare query for execute
+                                        $stmt = $con->prepare($query);
+                                        //posted values
+                                        $stmt->bindParam(":lastorderid", $last_order_id);
+                                        $stmt->bindParam(":productID", $productID[$i]);
+                                        $stmt->bindParam(":quantity", $quantity[$i]);
+                                        //execute the query
+                                        if ($stmt->execute()) {
+                                        }
+                                    } catch (PDOException $exception) {
+                                        die('ERROR: ' . $exception->getMessage());
                                     }
-                                } catch (PDOException $exception) {
-                                    die('ERROR: ' . $exception->getMessage());
                                 }
                             }
+                            echo "<div class='alert alert-success'>Record was saved.</div>";
+                        } else {
+                            echo "<div class='alert alert-danger'>Unable to save record.</div>";
                         }
-                        echo "<div class='alert alert-success'>Record was saved.</div>";
-                    } else {
-                        echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                    } catch (PDOException $exception) {
+                        die('ERROR: ' . $exception->getMessage());
                     }
-                } catch (PDOException $exception) {
-                    die('ERROR: ' . $exception->getMessage());
+                } else {
+                    echo "<div class='alert alert-danger'>Please Enter Your Quantity.</div>";
                 }
             } else {
-                foreach ($error as $value) {
-                    echo "<div class='alert alert-danger'>$value <br/></div>"; //start print error msg
-               }
+                echo "<div class='alert alert-danger'>Please Select Your Username.</div>";
             }
         }
 
         ?>
 
         <!-- html form here where the product information will be entered -->
-        <table class='table table-hover table-responsive table-bordered'>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <table class='table table-hover table-responsive table-bordered'>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
 
-                    <?php
+                <?php
 
-                    $query = "SELECT customerID, username FROM customer";
-                    $stmt = $con->prepare($query);
-                    // execute our query
-                    $stmt->execute();
-                    echo '<tr>
+                $query = "SELECT customerID, username FROM customer";
+                $stmt = $con->prepare($query);
+                // execute our query
+                $stmt->execute();
+                echo '<tr>
                             <td>Select username </td>
                             <td>
                             <div class="col">';
-                    echo "<select class='form_select' name='customerID' >";
-                    echo '<option selected>username</option>';
-                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                        extract($row);
-                        echo "<option value='" . $customerID . "' >" . $username . "</option>";
-                    }
-                    //for ($x = 1; $x <= 1; $x++) {
-                        
-                        try {
-                            // prepare select query
-                            $query = "SELECT * FROM products";
-                            $stmt = $con->prepare($query);
-                            
-                            // execute our query
-                            $stmt->execute();
-                            echo '<tr class="productrow">
+                echo "<select class='form_select' name='customerID' >";
+                echo '<option value="">username</option>';
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    extract($row);
+                    echo "<option value='" . $customerID . "' >" . $username . "</option>";
+                }
+                //for ($x = 1; $x <= 1; $x++) {
+
+                try {
+                    // prepare select query
+                    $query = "SELECT * FROM products";
+                    $stmt = $con->prepare($query);
+
+                    // execute our query
+                    $stmt->execute();
+                    echo '<tr class="productrow">
                                 <td>Select Product</td>
                                 <td>
                                 <div class="col">';
-                            echo "<select class='form_select' name='productID[]' >";
-                            echo '<option selected>Product</option>';
-                            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                                extract($row);
-                                echo "<option value='" . $productID . "' >" . $name . "</option>";
-                            }
-                            echo "</select>
+                    echo '<select class="form_select" name="productID[]" >';
+                    echo '<option value="">Product</option>';
+
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        extract($row);
+                        echo "<option value='" . $productID . "' >" . $name . "</option>";
+                    }
+                    echo "</select>
                             </div>
                                 Quantity
-                                <input type='number' name='quantity[]' class='form-control' value='' />";
-                        }
-                        // show error
-                        catch (PDOException $exception) {
-                            die('ERROR: ' . $exception->getMessage());
-                        }
-                    //}
-                    ?>
-
-                    <tr>
-                        <td></td>
-                        <td>
-                            <input type='submit' value='Save' class='btn btn-primary' />
-                            <a href='order_listing.php' class='btn btn-danger'>Back To Order List</a>
-                        </td>
-                    </tr>
-                </table>
-
+                                <input type='number' name='quantity[]' class='form-control' value='' /></td></tr>";
+                }
+                // show error
+                catch (PDOException $exception) {
+                    die('ERROR: ' . $exception->getMessage());
+                }
+                //}
+                ?>
                 <tr>
                     <td colspan="2">
                         <div>
@@ -153,13 +144,21 @@
                         </div>
                     </td>
                 </tr>
-            </form>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save' class='btn btn-primary' />
+                        <a href='order_listing.php' class='btn btn-danger'>Back To Order List</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
 
     </div>
     <!-- end .container -->
 
     <script>
-        document.addEventListener('click', function (event) {
+        document.addEventListener('click', function(event) {
             if (event.target.matches('.add')) {
                 var element = document.querySelector('.productrow');
                 var clone = element.cloneNode(true);
