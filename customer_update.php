5d617428590b7552050fc5caa89b3e20de20a8e2
LowZhong <lowzhong203056@dasein.edu.my>
2022-05-04 15:39:25 +0800

    customer update bootstrap done

--- customer_update.php
+++ customer_update.php
@@ -222,106 +222,107 @@
         <!--we have our html form here where new record information can be updated-->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?customerID={$customerID}"); ?>" method="post">
             <table class='table table-hover table-responsive table-bordered'>
-                <tr>
-                    <td>Username</td>
-                    <td><input name='username' <?php echo htmlspecialchars($username, ENT_QUOTES); ?> class='form-control' value="<?php echo $username; ?>" /></td>
-                </tr>
-                <tr>
-                    <td>Email</td>
-                    <td><input name='email' <?php echo htmlspecialchars($email, ENT_QUOTES); ?> class='form-control' value="<?php echo $email; ?>" /></td>
-                </tr>
-                <tr>
-                    <td>Password</td>
-                    <td><input name='password' <?php echo htmlspecialchars($password, ENT_QUOTES); ?> class='form-control' value="<?php echo $password; ?>" /></td>
-                </tr>
-                <tr>
-                    <td>Firstname</td>
-                    <td><input name='firstname' <?php echo htmlspecialchars($firstname, ENT_QUOTES); ?> class='form-control' value="<?php echo $firstname; ?>" /></td>
-                </tr>
-                <tr>
-                    <td>lastname</td>
-                    <td><input name='lastname' <?php echo htmlspecialchars($lastname, ENT_QUOTES); ?> class='form-control' value="<?php echo $lastname; ?>" /></td>
-                </tr>
-                <tr>
-                    <td>Gender</td>
-                    <td><input class="form-check-input" type="radio" name="gender" id="gender" <?php if ($gender == "male") echo "checked" ?> value="male">
-                        <label class="form-check-label" for="gender">
-                            Male
-                        </label>
-                        <input class="form-check-input" type="radio" name="gender" id="gender" <?php if ($gender == "female") echo "checked" ?> value="female">
-                        <label class="form-check-label" for="gender">
-                            Female
-                        </label>
-                    </td>
-                </tr>
+                <div class="mb-3">
+                    <tr class='border border-3'>
+                        <td class="border border-3 p-4">Username</td>
+                        <td class="input-group input-group-outline my-2"><input name='username' <?php echo htmlspecialchars($username, ENT_QUOTES); ?> class='form-control' value="<?php echo $username; ?>" /></td>
+                    </tr>
+                    <tr class='border border-3'>
+                        <td class="border border-3 p-4">Email</td>
+                        <td class="input-group input-group-outline my-2"><input name='email' <?php echo htmlspecialchars($email, ENT_QUOTES); ?> class='form-control' value="<?php echo $email; ?>" /></td>
+                    </tr>
+                    <tr class='border border-3'>
+                        <td class="border border-3 p-4">Password</td>
+                        <td class="input-group input-group-outline my-2"><input name='password' <?php echo htmlspecialchars($password, ENT_QUOTES); ?> class='form-control' value="<?php echo $password; ?>" /></td>
+                    </tr>
+                    <tr class='border border-3'>
+                        <td class="border border-3 p-4">Firstname</td>
+                        <td class="input-group input-group-outline my-2"><input name='firstname' <?php echo htmlspecialchars($firstname, ENT_QUOTES); ?> class='form-control' value="<?php echo $firstname; ?>" /></td>
+                    </tr>
+                    <tr class='border border-3'>
+                        <td class="border border-3 p-4">lastname</td>
+                        <td class="input-group input-group-outline my-2"><input name='lastname' <?php echo htmlspecialchars($lastname, ENT_QUOTES); ?> class='form-control' value="<?php echo $lastname; ?>" /></td>
+                    </tr>
+                    <tr class='border border-3'>
+                        <td class="border border-3 p-4">Gender</td>
+                        <td class="p-4"><input class="form-check-input" type="radio" name="gender" id="gender" <?php if ($gender == "male") echo "checked" ?> value="male">
+                            <label class="form-check-label" for="gender">
+                                Male
+                            </label>
+                            <input class="form-check-input" type="radio" name="gender" id="gender" <?php if ($gender == "female") echo "checked" ?> value="female">
+                            <label class="form-check-label" for="gender">
+                                Female
+                            </label>
+                        </td>
+                    </tr>
 
-                <tr>
-                    <td>Account Status</td>
-                    <td><input class="form-check-input" type="radio" name="status" id="status" <?php if ($status == "active") echo "checked" ?> value="active">
-                        <label class="form-check-label" for="status">
-                            Active
-                        </label>
-                        <input class="form-check-input" type="radio" name="status" id="status" <?php if ($status == "disabled") echo "checked" ?> value="disabled">
-                        <label class="form-check-label" for="status">
-                            Disabled
-                        </label>
-                    </td>
-                </tr>
+                    <tr class='border border-3'>
+                        <td class="border border-3 p-4">Account Status</td>
+                        <td class="p-4"><input class="form-check-input" type="radio" name="status" id="status" <?php if ($status == "active") echo "checked" ?> value="active">
+                            <label class="form-check-label" for="status">
+                                Active
+                            </label>
+                            <input class="form-check-input" type="radio" name="status" id="status" <?php if ($status == "disabled") echo "checked" ?> value="disabled">
+                            <label class="form-check-label" for="status">
+                                Disabled
+                            </label>
+                        </td>
+                    </tr>
 
-                <tr>
-                    <td>Date Of Birth</td>
-                    <!--day-->
-                    <td>
-                        <?php
+                    <tr class='border border-3'>
+                        <td class="border border-3 p-4">Date Of Birth</td>
+                        <!--day-->
+                        <td class="p-4">
+                            <?php
 
-                        echo '<select id="day" name="day">' . "\n";
-                        for ($day = 1; $day <= 31; $day++) {
-                            $selected = ($dobday == $day ? ' selected' : '');
-                            echo '<option value="' . $day . '"' . $selected . '>' . $day . '</option>' . "\n";
+                            echo '<select id="day" name="day">' . "\n";
+                            for ($day = 1; $day <= 31; $day++) {
+                                $selected = ($dobday == $day ? ' selected' : '');
+                                echo '<option value="' . $day . '"' . $selected . '>' . $day . '</option>' . "\n";
+                            }
+                            echo '</select>' . "\n";
+                            ?>
+
+                            <!--month-->
+                            <?php
+
+                            echo '<select id="month" name="month">' . "\n";
+                            for ($month = 1; $month <= 12; $month++) {
+                                $selected = ($dobmonth == $month ? ' selected' : '');
+                                echo '<option value="' . $month . '"' . $selected . '>' . date('F', mktime(0, 0, 0, $month)) . '</option>' . "\n";
+                            }
+                            echo '</select>' . "\n";
+                            if ((isset($_GET['month'])) && ($value))
+                            ?>
+
+                            <!--year-->
+                            <?php
+                        $year_start  = 2022;
+                        $birthdate = 2022;
+
+                        echo '<select id="year" name="year">' . "\n";
+                        for ($year = $year_start; $year >= 1990; $year--) {
+                            $selected = ($dobyear == $year ? ' selected' : '');
+                            echo '<option value="' . $year . '"' . $selected . '>' . $year . '</option>' . "\n";
                         }
                         echo '</select>' . "\n";
-                        ?>
+                            ?>
+                        </td>
+                    <tr>
 
-                        <!--month-->
-                        <?php
-
-                        echo '<select id="month" name="month">' . "\n";
-                        for ($month = 1; $month <= 12; $month++) {
-                            $selected = ($dobmonth == $month ? ' selected' : '');
-                            echo '<option value="' . $month . '"' . $selected . '>' . date('F', mktime(0, 0, 0, $month)) . '</option>' . "\n";
-                        }
-                        echo '</select>' . "\n";
-                        if ((isset($_GET['month'])) && ($value))
-                        ?>
-
-                        <!--year-->
-                        <?php
-                    $year_start  = 2022;
-                    $birthdate = 2022;
-
-                    echo '<select id="year" name="year">' . "\n";
-                    for ($year = $year_start; $year >= 1990; $year--) {
-                        $selected = ($dobyear == $year ? ' selected' : '');
-                        echo '<option value="' . $year . '"' . $selected . '>' . $year . '</option>' . "\n";
-                    }
-                    echo '</select>' . "\n";
-                        ?>
-                    </td>
-                <tr>
-
-                <tr>
-                    <td>Update Your Profile Picture</td>
-                    <td>
-                        <input type="file" name="image" />
-                    </td>
-                </tr>
-                <td></td>
-                <td>
+                    <tr class='border border-3'>
+                        <td class="border border-3 p-4">Update Your Profile Picture</td>
+                        <td class="p-4">
+                            <input type="file" name="image" />
+                        </td>
+                    </tr>
+                </div>
+            </table>
+            <div class="d-flex justify-content-end gap-2">
+                <td class="d-flex justify-content-end gap-2">
                     <input type='submit' value='Save Changes' class='btn btn-primary' />
                     <a href='customer_read.php' class='btn btn-danger'>Back to Customer List</a>
-                </td>
-                </tr>
-            </table>
+            </div>
         </form>
 
 
