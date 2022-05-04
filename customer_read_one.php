a10130da5a52eba40e96d690c4311df95718e792
LowZhong <lowzhong203056@dasein.edu.my>
2022-05-04 15:29:37 +0800

    bootstrap customer read one

--- customer_read_one.php
+++ customer_read_one.php
@@ -3,15 +3,37 @@
 
 <head>
     <title>PDO - Read One Record - PHP CRUD Tutorial</title>
-    <!-- Latest compiled and minified Bootstrap CSS → -->
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900%7CRoboto+Slab:400,700" />
+    <!-- Nucleo Icons -->
+    <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="assets/css/styles.css" rel="stylesheet" />
 </head>
 
 <body>
     <!-- container -->
-    <div class="container">
+    <div class="container ms-5">
+        <?php include 'navbar/navbar.php'; ?>
+        <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+            <div class="container-fluid py-1 px-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Customer Details</li>
+                    </ol>
+                </nav>
+            </div>
+        </nav>
         <div class="page-header">
-            <?php include 'navbar/navbar.php'; ?>
             <h1>Customer Details</h1>
         </div>
 
@@ -66,58 +88,62 @@
         <!-- HTML read one record table will be here -->
 
         <table class='table table-hover table-responsive table-bordered'>
-            <tr>
-                <td>Customer ID</td>
-                <td><?php echo htmlspecialchars($customerID, ENT_QUOTES);  ?></td>
-            </tr>
-            <tr>
-                <td>Username</td>
-                <td><?php echo htmlspecialchars($username, ENT_QUOTES);  ?></td>
-            </tr>
-            <tr>
-                <td>Email</td>
-                <td><?php echo htmlspecialchars($email, ENT_QUOTES);  ?></td>
-            </tr>
-            <tr>
-                <td>Password</td>
-                <td><?php echo htmlspecialchars($password, ENT_QUOTES);  ?></td>
-            </tr>
-            <tr>
-                <td>Firstname</td>
-                <td><?php echo htmlspecialchars($firstname, ENT_QUOTES);  ?></td>
-            </tr>
-            <tr>
-                <td>Lastname</td>
-                <td><?php echo htmlspecialchars($lastname, ENT_QUOTES);  ?></td>
-            </tr>
-            <tr>
-                <td>Gender</td>
-                <td><?php echo htmlspecialchars($gender, ENT_QUOTES);  ?></td>
-            </tr>
-            <tr>
-                <td>Status</td>
-                <td><?php echo htmlspecialchars($status, ENT_QUOTES);  ?></td>
-            </tr>
-            <tr>
-                <td>Date Of Birth</td>
-                <td><?php echo htmlspecialchars($birthdate, ENT_QUOTES);  ?></td>
-            </tr>
-            <tr>
-                <td>Star Sign</td>
-                <td><?php starsign($month, $day);  ?></td>
-            </tr>
-            <tr>
-                <td>Animal Year</td>
-                <td><?php animalYear($year); ?></td>
-            </tr>
-            <tr>
-                <td>Images</td>
-                <td><?php echo " ". user_img($user_image) . " "?></td>
-            </tr>
-            <a href='customer_read.php' class='btn btn-danger'>Back to Customer List</a>
-            </td>
-            </tr>
+            <div class="mb-3">
+                <tr class='border border-3'>
+                    <td class='border border-3'>Customer ID</td>
+                    <td><?php echo htmlspecialchars($customerID, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Username</td>
+                    <td><?php echo htmlspecialchars($username, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Email</td>
+                    <td><?php echo htmlspecialchars($email, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Password</td>
+                    <td><?php echo htmlspecialchars($password, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Firstname</td>
+                    <td><?php echo htmlspecialchars($firstname, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Lastname</td>
+                    <td><?php echo htmlspecialchars($lastname, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Gender</td>
+                    <td><?php echo htmlspecialchars($gender, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Status</td>
+                    <td><?php echo htmlspecialchars($status, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr>
+                    <td class='border border-3'>Date Of Birth</td>
+                    <td><?php echo htmlspecialchars($birthdate, ENT_QUOTES);  ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Star Sign</td>
+                    <td><?php starsign($month, $day);  ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Animal Year</td>
+                    <td><?php animalYear($year); ?></td>
+                </tr>
+                <tr class='border border-3'>
+                    <td class='border border-3'>Images</td>
+                    <td><?php echo " " . user_img($user_image) . " " ?></td>
+                </tr>
+            </div>
+
         </table>
+        <div class="d-flex justify-content-end gap-2">
+            <td class="d-flex justify-content-end gap-2">
+                <a href='customer_read.php' class='btn btn-danger'>Back to Customer List</a>
+        </div>
         <!--we have our html table here where the record will be displayed-->
 
     </div>
